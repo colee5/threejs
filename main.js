1ae675b3ee95b00654c05d05ad5c1e5be89a6216
Oliver Gjorgjiev <oliver.gorgiev11@gmail.com>
2024-11-07 23:11:16 +0100

    feat: add dat.gui & experiment

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-// All three js functionalities - may not be a good practice
 import * as THREE from "three";
-// import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls.js";
+import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 // We initialize an instance, tool that three.js
 // uses to allocate space on the webpage
 const renderer = new THREE.WebGLRenderer();
+const gui = new dat.GUI();
+
+// These are options which are handled by the GUI to give
+// us the colorpicker and the switch, etc..
+const options = {
+  color: 0x0000ff,
+  wireframe: false,
+  speed: 0.01,
+};
+
+// We append the helpers into the GUI
+gui.addColor(options, "color").onChange(function (e) {
+  sphereMesh.material.color.set(e);
+});
+
+gui.add(options, "wireframe").onChange(function (e) {
+  sphereMesh.material.wireframe = e;
+});
+
+gui.add(options, "speed", 0, 0.01);
+
+let step = 0;
 
 // We allocate space to it by giving the entire
 // width and height property of our website window
@@ -45,6 +66,11 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
 
+box.translateX(5);
+
+box.rotateX(Math.PI / 2);
+
+box.scale.set(1, 2, 1);
 // Plane scene - plane is a two dimensional surface, more like a floor
 
 const planeGeometry = new THREE.PlaneGeometry(15, 15);
@@ -56,16 +82,25 @@ scene.add(planeMesh);
 
 planeMesh.rotation.x = -0.5 * Math.PI;
 
-const sphereGeometry = new THREE.SphereGeometry(2);
+// The default values for the horizontal segments is 32
+// and 16 for the vertical segments
+// THREE.SphereGeometry(2) is same as
+// THREE.SphereGeometry(2, 32, 16)
+const sphereGeometry = new THREE.SphereGeometry(2, 60, 60);
+
+// MeshStandardMaterial requires light in order to be able to be seen
+// Just like in real life, If no light everything will be dark
 const sphereMaterial = new THREE.MeshBasicMaterial({
   color: 0x0000ff,
+  wireframe: true,
 });
+
 const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphereMesh);
 
 function animate(time) {
-  box.rotation.x = time / 1000;
-  box.rotation.y = time / 1000;
+  step += options.speed;
+  sphereMesh.position.y = 3 * Math.abs(Math.sin(step));
   renderer.render(scene, camera);
 }
 

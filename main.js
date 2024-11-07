c534d845d589dacab5d8e0a86669767fef40ef41
Oliver Gjorgjiev <oliver.gorgiev11@gmail.com>
2024-11-07 14:36:30 +0100

    fix renderer typo

--- main.js
+++ main.js
@@ -3,11 +3,30 @@ import * as THREE from "three";
 
 // We initialize an instance, tool that three.js
 // uses to allocate space on the webpage
-const rendered = new THREE.WebGL3DRenderer();
+const renderer = new THREE.WebGLRenderer();
 
 // We allocate space to it by giving the entire
 // width and height property of our website window
-rendererReference.setSize(window.innerHeight, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // We inject that space into basically a <canvas> element
 document.body.appendChild(renderer.domElement);
+
+// Creating a scene
+const scene = new THREE.Scene();
+
+// Initialize a perspective Camera which takes 4 props
+const camera = new THREE.PerspectiveCamera(
+  75, // Field of view - anything between 40 and 8 are sufficient
+  window.innerWidth / window.innerHeight, // Aspect ratio which we calculate by dividing our canvas size
+  0.1, // Near clipping plane
+  1000 // Far clipping plane
+);
+
+camera.position.z = 5;
+// We render and link scene to the camera
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+renderer.render(scene, camera);
